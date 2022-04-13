a5f5337a5236648faf7d68d6aa683927b738a6db
vinivasq <vinivasq@hotmail.com>
2022-04-13 17:22:08 -0300

    condicionais e ternarios

--- My first project/Assets/Scripts/condicionais.cs
+++ My first project/Assets/Scripts/condicionais.cs
@@ -4,11 +4,33 @@ using UnityEngine;
 
 public class condicionais : MonoBehaviour
 {
-    
+    int moedas = 120;
+    int item = 100;
+    string result;
 
     void Start()
     {
+        /*
+        if (moedas == item)  // poderia usar um >= porém separei para exemplificar o else-if
+        {
+            print ("Item adquirido!"); 
+        }   
+
+        else if (moedas > item) {
+            print ("Item adquirido e ainda sobrou uma merreca");
+        }
+
+        else
+        {
+            print ("Moedas insuficientes");
+        }
+        */
         
+        // Operadores ternarios
+        // CONDIÇÃO?TRUE:FALSE
+
+        result = (moedas >= item)?"Item adquirido!":"Moedas insuficientes";
+        print (result);
     }
 
     void Update()
