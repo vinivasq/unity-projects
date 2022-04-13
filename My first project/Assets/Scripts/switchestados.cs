deff65b06de6c5359b2872866bcbde867320a6f6
vinivasq <vinivasq@hotmail.com>
2022-04-13 17:40:03 -0300

    switch de estados

--- My first project/Assets/Scripts/switchestados.cs
+++ My first project/Assets/Scripts/switchestados.cs
@@ -4,11 +4,30 @@ using UnityEngine;
 
 public class switchestados : MonoBehaviour
 {
-    
+    // Estados do inimigo
+    // status 1: coragem
+    // status 2: calmo
+    // status 3: covarde
+
+    int status = 1;
 
     void Start()
     {
-        
+        switch (status)
+        {
+            default:
+                print ("Nenhum estado disponível");
+                break;
+            case 1:
+                print ("Avance no heroi");
+                break;
+            case 2:
+                print ("Mate se puder");
+                break;
+            case 3:
+                print ("Fuja e se proteja!");
+                break;
+        }
     }
 
     void Update()
